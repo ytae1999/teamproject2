7b4a0f89c5843f6f0949945873b1f1714f12aac1
태영준 <ytae1999@naver.com>
2019-10-07 14:03:47 +0900

    주석문 추가

--- MyApp.java
+++ MyApp.java
@@ -11,23 +11,23 @@ public class MyApp
 {
     public static void main(String[] args){
         try{
-            Scanner scanner = new Scanner(new FileReader("c:\\Temp\\inputData20191007"));
+            Scanner scanner = new Scanner(new FileReader("c:\\Temp\\inputData20191007")); //파일 입력
         }
         catch(IOException e){
             System.out.println("오류"); 
         }
         HashMap<String, Integer> h = new HashMap<String, Integer>();
-        // while(scanner.hasNext()){
+        // while(scanner.hasNext()){ // 한라인씩 읽기
 
         Set<String> keys = h.keySet();
         Iterator<String> it = keys.iterator();
         
-        int sum = 0;
-        while(it.hasNext()){
+        int sum = 0; // sum 초기화 
+        while(it.hasNext()){ 
             String key = it.next();
-            sum = sum + h.get(key);
+            sum = sum + h.get(key); //sum의 값들을 계속 더해간다
         }
-        System.out.println("총점 : " + sum);
-        System.out.println("평균 : " + (sum/h.size()));
+        System.out.println("총점 : " + sum); //총점 출력 
+        System.out.println("평균 : " + (sum/h.size())); //평균 출력
     }
 }
