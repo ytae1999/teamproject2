c1756def1ec50bd93643996523c879ebcde84408
Hwanghuntae <ghkdgnsxo2484@naver.com>
2019-10-07 13:39:38 +0900

    main함수 생성 FIleReader 생성

---
+++ MyApp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * 여기에 aaaa 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class MyApp
+{
+    public static void main(String[] args){
+        try{
+            Scanner scanner = new Scanner(new FileReader("c:\\Temp\\inputData20191007"));
+        }
+        catch(IOException e){
+            System.out.println("오류");
+        }
+    }
+}
